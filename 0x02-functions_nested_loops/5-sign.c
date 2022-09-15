fd1d2e8d71e1d67cc935ce025528c204692f1883
programmer101-droid <sirdaveharry@gmail.com>
2022-09-15 05:54:07 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include main.h
+/**
+ * print_sign - function to check for the sign of a number
+ * @n: is the int passed as function argument
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
