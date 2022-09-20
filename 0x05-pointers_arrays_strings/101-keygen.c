076646fd7f5688fc3261338d689fee55b83db95d
programmer101-droid <sirdaveharry@gmail.com>
2022-09-20 03:39:48 +0300

    pointers, arrays, strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
-
 /**
- * main - entry point
+ * main - entry point generates a random password
  *
- * Return: generated password
+ * Return: the generated password
  */
-
 int main(void)
 {
-	char c;
-	int x;
+char c;
+int x;
 
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
-
-	return (0);
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
 }
